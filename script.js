1af79637b6351b433213062500e265e5642052ea
Coralie Turcius <coralie.turcius@gmail.com>
2022-11-04 15:25:56 +0100

    year-frontend

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 fetch('https://year-vercel-backend.vercel.app/year')
  .then(response => response.json())
  .then(data => {
+  console.log(data);
   document.querySelector('#year').textContent = data.year;
  });
